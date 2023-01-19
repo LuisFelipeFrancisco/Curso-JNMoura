bd3a02a55efdd55ae623763178b87ced19f32523
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2023-01-19 11:34:14 -0300

    feat: HelloWorld

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program // Create a class called Program
+    {
+        static void Main(string[] args) // Create a method called Main
+        {
+            Console.WriteLine("Hello World!"); // Write "Hello World!" to the console
+            Console.ReadKey(); // Pause at the end
+        }
+    }
+}
