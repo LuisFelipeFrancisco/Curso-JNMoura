e3f29a6b512af0a496de67669461c4f3e13be44a
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2023-01-20 11:09:02 -0300

    feat: Sum of 2 numbers

--- basics/sum/Program.cs
+++ basics/sum/Program.cs
@@ -10,16 +10,37 @@ namespace sum
     {
         static void Main(string[] args)
         {
-            /* Sum os all the numbers in a range */
-            int sum = 0;
-            int start = 1;
-            int end = 5;
-            for (int i = start; i <= end; i++)
-            {
-                sum += i;
-            }
-            Console.WriteLine("Sum of all the numbers from {0} to {1} is {2}", start, end, sum);
-            Console.ReadLine();
+            /* Sum of 2 numbers */
+
+            int num1, num2, sum;
+
+            Console.Write("Enter first number: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            sum = num1 + num2;
+
+            Console.WriteLine("Sum of {0} and {1} is {2}", num1, num2, sum);
+            Console.WriteLine($"Sum of {num1} and {num2} is {sum}");
+
+            Console.Write("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
+
+
+/* {0} is for replacing the first argument in the string and so on. 
+* The arguments are passed in the order they are written in the string.
+* bool is a data type that can hold only true or false. size of bool is 1 byte.
+* int is a data type that can hold only integer values. size of int is 4 bytes.
+* double is a data type that can hold only decimal values. size of double is 8 bytes.
+* char is a data type that can hold only a single character. size of char is 2 bytes.
+* string is a data type that can hold a string of characters. size of string is 2 bytes per character.
+* implicit conversion is when the compiler automatically converts one data type to another.
+* .Parse is a method that converts a string to a number.
+* .Convert is a method that converts one data type to another.
+* Template Strings are strings that can contain placeholders for variables.
+*/
