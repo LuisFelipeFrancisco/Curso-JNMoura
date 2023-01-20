e9c0e304411fdc56ff58a40b0f61de67ba2a099a
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2023-01-20 07:32:09 -0300

    fix: naming

--- basics/sum/Properties/AssemblyInfo.cs
+++ basics/sum/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associadas a um assembly.
-[assembly: AssemblyTitle("somar")]
+[assembly: AssemblyTitle("sum")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("somar")]
+[assembly: AssemblyProduct("sum")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
-[assembly: Guid("240f0a5e-46b5-4ef7-acad-adac9f3d6aa7")]
+[assembly: Guid("956ed190-d65a-4b1c-907c-e17fb8787240")]
 
 // As informações da versão de um assembly consistem nos quatro valores a seguir:
 //
