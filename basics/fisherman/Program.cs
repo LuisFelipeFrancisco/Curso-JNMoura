8a8c7f8059817c7d655464bd5e5dc8fe2e77b66e
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2023-01-25 12:12:53 -0300

    feat: conditional and constants

---
+++ basics/fisherman/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+/*
+* A fisherman bought a microcomputer to control the daily yield of his work.
+* Every time he brings a weight of fish greater than the established by the fishing regulation of the state of São Paulo
+* (50 kilos) must pay a fine of R $ 4.00 per kilo excess.
+* So, make a program that reads the weight of fish and check if there is excess.
+* If there is, the program should also calculate the value of the fine that the fisherman should pay.
+* In the end, the program should print the excess and the fine paid by the fisherman.
+*/
+
+
+namespace fisherman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double fine = 4.00;
+            const double kgLimit = 50.00;
+
+            Console.WriteLine("Enter the weight of fish: ");
+            double fishWeight = double.Parse(Console.ReadLine());
+
+            double excess = 0;
+            double fineValue = 0;
+
+            if (fishWeight > kgLimit)
+            {
+                excess = fishWeight - kgLimit;
+                fineValue = excess * fine;
+            }
+
+            Console.WriteLine($"Excess: {excess:0.00} kg");
+            Console.WriteLine($"Fine: {fineValue:C2}");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ basics/password/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/* Make a program that receives a password and tests if it is equal to 904087.
+* If the password is correct write "Access granted",
+* otherwise, write the message "You don't have access to the system".
+*/
+
+namespace password
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string password = "904087";
+
+            Console.WriteLine("Enter the password: ");
+            string userPassword = Console.ReadLine();
+
+            if (userPassword == password)
+            {
+                Console.WriteLine("Access granted");
+            }
+            else
+            {
+                Console.WriteLine("You don't have access to the system");
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
+
+/* Conditional statements are used to perform different actions based on different conditions.
+* If the condition is true, the code inside the if block is executed.
+* Const is a keyword that is used to declare a constant variable.
+*/
