358d51c29b2dec37bfc32cc0d57013bf8ea0f06d
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2023-01-26 18:33:40 -0300

    feat: nested statement, logical operators

---
+++ basics/election/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+/* 
+Em uma eleição presidencial, existem quatro candidatos. Os votos são informados através de códigos.
+Os códigos utilizados são:
+• 1,2,3,4 votos para os respectivos candidatos;
+• 5 voto nulo;
+• 6 voto em branco.
+
+Assim, escreva um programa para receber um único voto e imprima:
+• Se o usuário votou em um candidato: "Voto contabilizado com sucesso..."
+• Se o usuário votou nulo: "Voto nulo contabilizado..."
+• Se o usuário votou em branco: "Voto branco contabilizado..."
+
+@ Caso o usuário digite um código inválido, exiba a mensagem "Voto inválido"
+*/
+
+namespace election
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Election");
+            Console.WriteLine("Enter your vote: ");
+            var vote = int.Parse(Console.ReadLine());
+
+            if (vote > 0 && vote < 5)
+            {
+                Console.WriteLine("Vote counted successfully...");
+            }
+            else if (vote == 5)
+            {
+                Console.WriteLine("Null vote counted...");
+            }
+            else if (vote == 6)
+            {
+                Console.WriteLine("Blank vote counted...");
+            }
+            else
+            {
+                Console.WriteLine("Invalid vote");
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ basics/school/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+/* Faça um programa para ler três notas e o número de faltas de um aluno e escrever qual a sua situação final: 
+* Aprovado, Reprovado por Falta ou Reprovado por Média. 
+* A média para aprovação é 7,0 e o limite de faltas é 25% do total de aulas. 
+* O número de aulas ministradas no semestre foi de 80. A reprovação por falta sobrepõe a reprovação por Média.
+*/
+
+namespace school
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your absences: ");
+            int absences = int.Parse(Console.ReadLine());
+
+            if (absences > 20)
+            {
+                Console.WriteLine("You are repproved by absences");
+            }
+            else
+            {
+                Console.WriteLine("Enter your first grade: ");
+                double grade1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter your second grade: ");
+                double grade2 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter your third grade: ");
+                double grade3 = double.Parse(Console.ReadLine());
+
+                double average = (grade1 + grade2 + grade3) / 3;
+
+                if (average >= 7)
+                {
+                    Console.WriteLine("You are approved");
+                }
+                else
+                {
+                    Console.WriteLine("You are repproved by average");
+                }
+            }
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
