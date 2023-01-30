e106db629b1293a9a81de3fb138944a010696dd7
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2023-01-30 20:04:26 -0300

    feat: exercises

---
+++ basics/ages/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+/*
+ * Faça um programa que solicite a idade de várias pessoas e mostre:
+ * • Total de pessoas com menos de 21 anos.
+ * • Total de pessoas com mais de 50 anos.
+ * • A média de idade das pessoas.
+ * Importante: O programa termina quando a idade informada for -1 (Menos 1)
+*/
+namespace ages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age, under21 = 0, over50 = 0, sum = 0, count = 0;
+            double average;
+            Console.WriteLine("Enter the age: ");
+            age = int.Parse(Console.ReadLine());
+            while (age != -1)
+            {
+                if (age < 21)
+                {
+                    under21++;
+                }
+                if (age > 50)
+                {
+                    over50++;
+                }
+                sum += age;
+                count++;
+                Console.WriteLine("Enter the age: ");
+                age = int.Parse(Console.ReadLine());
+            }
+            average = (double)sum / count;
+            Console.WriteLine("Total of people under 21 years old: " + under21);
+            Console.WriteLine("Total of people over 50 years old: " + over50);
+            Console.WriteLine("The average of the ages is: " + average);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ basics/greaterAndSum/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+/*
+ * Faça um programa que leia 5 números inteiros informados pelo usuário e, 
+ * no final, mostre o maior número e, também, a soma deles.
+*/
+
+namespace greaterAndSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3, num4, num5, sum, greater;
+            Console.WriteLine("Enter the first number: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the third number: ");
+            num3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the fourth number: ");
+            num4 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the fifth number: ");
+            num5 = int.Parse(Console.ReadLine());
+
+            sum = num1 + num2 + num3 + num4 + num5;
+            greater = num1;
+
+            if (num2 > greater)
+            {
+                greater = num2;
+            }
+            if (num3 > greater)
+            {
+                greater = num3;
+            }
+            if (num4 > greater)
+            {
+                greater = num4;
+            }
+            if (num5 > greater)
+            {
+                greater = num5;
+            }
+
+            Console.WriteLine("The greater number is: " + greater);
+            Console.WriteLine("The sum of the numbers is: " + sum);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
