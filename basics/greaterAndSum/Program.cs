7996020b39727a2144863a9dfa92183b8517bb6f
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2023-02-01 09:36:07 -0300

    refactor: refactoring production code

--- basics/greaterAndSum/Program.cs
+++ basics/greaterAndSum/Program.cs
@@ -10,25 +10,36 @@ namespace greaterAndSum
     {
         static void Main(string[] args)
         {
-            int greater = 0, sum = 0;
+            const int qtdNumbers = 5;
+            int greater = int.MinValue, sum = 0, i = 1;
+
+            #region "Mine"
+            /*
             for (int i = 1; i < 6; i++)
             {
                 Console.WriteLine($"Enter the {i}º integer number:");
                 int number = int.Parse(Console.ReadLine());
-                if (i == 1)
+                
+                if (number > greater)
                 {
                     greater = number;
-                    sum = number;
                 }
-                else
-                {
-                    if (number > greater)
-                    {
-                        greater = number;
-                    }
-                    sum += number;
-                }
+                sum += number;
             }
+            */
+            #endregion
+
+            do {
+                Console.WriteLine($"Enter the {i}º integer number:");
+                int number = int.Parse(Console.ReadLine());
+                
+                if (number > greater)
+                {
+                    greater = number;
+                }
+                sum += number;
+                i++;
+            } while (i <= qtdNumbers);
 
             Console.WriteLine($"The greater number is {greater}");
             Console.WriteLine($"The sum of the numbers is {sum}");
