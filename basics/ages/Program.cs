4af5959335b463e74d111e75a9b110cb41e01946
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2023-02-01 11:22:35 -0300

    refactor: invalid age, division by 0

--- basics/ages/Program.cs
+++ basics/ages/Program.cs
@@ -5,6 +5,7 @@
  * • Total de pessoas com mais de 50 anos.
  * • A média de idade das pessoas.
  * Importante: O programa termina quando a idade informada for -1 (Menos 1)
+ * @ Numeros negativos diferentes de -1 serão ignorados.
 */
 namespace ages
 {
@@ -14,26 +15,35 @@ namespace ages
         {
             int age, under21 = 0, over50 = 0, sum = 0, count = 0;
             double average;
-
-            Console.WriteLine("Enter the age: ");
-            age = int.Parse(Console.ReadLine());
-            
-            while (age != -1)
+            do
             {
-                if (age < 21)
-                {
-                    under21++;
-                }
-                if (age > 50)
-                {
-                    over50++;
-                }
-                sum += age;
-                count++;
-                Console.WriteLine("Enter the age: ");
+                Console.Write("Enter the age: ");
                 age = int.Parse(Console.ReadLine());
-            }
-            average = (double)sum / count;
+                if (age == -1)
+                {
+                    break;
+                }
+                else if (age < 0)
+                {
+                    Console.WriteLine("Invalid age. Try again.");
+                    continue;
+                }
+                else
+                {
+                    if (age < 21)
+                    {
+                        under21++;
+                    }
+                    else if (age > 50)
+                    {
+                        over50++;
+                    }
+                    sum += age;
+                    count++;
+                }
+            } while (true);
+
+            average = (double)sum / (count != 0 ? count : 1); // ternary operator (condition ? true : false) 
             
             Console.WriteLine($"Total of people under 21 years old: {under21}");
             Console.WriteLine($"Total of people over 50 years old: {over50}");
