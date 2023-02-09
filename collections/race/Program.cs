f8afac6eb8c3c77bde6bd4cf538e40f234a63f45
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2023-02-09 14:38:31 -0300

    feat: TryParse and List

--- collections/race/Program.cs
+++ collections/race/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 /*
  * Durante uma corrida de automóveis com N voltas de duração foram anotados para um piloto, na ordem, os tempos
@@ -19,6 +17,44 @@ namespace race
     {
         static void Main(string[] args)
         {
+            List<double> tempos = new List<double>();
+            double melhorTempo = 0;
+            int voltaMelhorTempo = 0;
+            double tempoMedio = 0;
+
+            Console.WriteLine("Digite o tempo da volta ou 0 para sair: ");
+            while (true)
+            {
+                Console.Write($"{tempos.Count + 1}º tempo: ");
+                if (double.TryParse(Console.ReadLine(), out double tempo))
+                {
+                    if (tempo == 0)
+                    {
+                        break;
+                    }
+                    else 
+                    {
+                        tempos.Add(tempo);
+                        tempoMedio += tempo;
+                        if (melhorTempo == 0 || tempo < melhorTempo)
+                        {
+                            melhorTempo = tempo;
+                            voltaMelhorTempo = tempos.Count;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Tempo inválido. Digite novamente: ");
+                }
+            }
+
+            Console.WriteLine($"Melhor tempo: {melhorTempo}");
+            Console.WriteLine($"Volta em que o melhor tempo ocorreu: {voltaMelhorTempo}");
+            Console.WriteLine($"Tempo médio das {tempos.Count} voltas: {tempoMedio / tempos.Count}");
+
+            Console.WriteLine("Pressione qualquer tecla para sair...");
+            Console.ReadKey();
         }
     }
 }
