a6f2c2c8ce25d80d078ef0ab02b2d477a2558fac
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2023-02-08 23:34:01 -0300

    feat: activities

---
+++ collections/race/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Durante uma corrida de automóveis com N voltas de duração foram anotados para um piloto, na ordem, os tempos
+ * registrados em cada volta. Fazer um programa para ler os tempos das N voltas, calcular e imprimir:
+ * a) melhor tempo
+ * b) volta em que o melhor tempo ocorreu;
+ * c) tempo médio das N voltas; 
+ * Importante: Para encerrar o programa o usuário indica que não deseja mais informar o tempo da volta.
+*/
+
+namespace race
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
