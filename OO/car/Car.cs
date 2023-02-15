44a3015c7c459532fd3989b80d8641b71fbd36e0
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2023-02-15 11:59:06 -0300

    feat: Object-oriented programming

---
+++ OO/car/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace car
+{
+    internal class Car
+    {
+        private int DoorsQuantity { get; set; }
+        private string Model { get; set; }
+        private string Manufacturer { get; set; }
+        private int Year { get; set; }
+        private string Color { get; set; }
+        private int Speed { get; set; }
+
+        public Car(int doorsQuantity, string model, string manufacturer, int year, string color, int speed)
+        {
+            DoorsQuantity = doorsQuantity;
+            Model = model;
+            Manufacturer = manufacturer;
+            Year = year;
+            Color = color;
+            Speed = speed;
+        }
+
+        public void Accelerate()
+        {
+            Console.WriteLine($"The {Color} {Model} from {Manufacturer} is accelerating... Vrum vrum!");
+            Speed += 10;
+        }
+
+        public void Brake()
+        {
+            Speed -= 10;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"The {Color} {Model} from {Manufacturer} has {DoorsQuantity} doors and is going {Speed} km/h.");
+        }
+    }
+}
---
+++ OO/car/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace car
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car civic = new Car(4, "Civic", "Honda", 2019, "Black", 0);
+            Car celta = new Car(4, "Celta", "Chevrolet", 2018, "White", 0);
+            Car fusca = new Car(2, "Fusca", "Volkswagen", 1970, "Yellow", 0);
+
+            civic.ShowInfo();
+            celta.ShowInfo();
+            fusca.ShowInfo();
+
+            civic.Accelerate();
+            civic.Accelerate();
+
+            celta.Accelerate();
+            celta.Accelerate();
+            celta.Accelerate();
+
+            fusca.Accelerate();
+
+            civic.ShowInfo();
+            celta.ShowInfo();
+            fusca.ShowInfo();
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
