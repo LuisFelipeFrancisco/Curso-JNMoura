f450d53ad644dc386d1825ec1ee7123c509522c4
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2023-03-10 10:52:25 -0300

    feat: TRUNCATE, DROP, FOREIGN KEY

--- SQLServer/DDL.sql
+++ SQLServer/DDL.sql
@@ -22,3 +22,48 @@ CREATE TABLE medico (
     datanascimento DATE, -- DATE => data formato AAAA-MM-DD
     CONSTRAINT pk_medico PRIMARY KEY (codigo) -- CONSTRAINT => restrição, PRIMARY KEY => chave primaria, (codigo) => campo que será a chave primaria
 );
+
+TRUNCATE TABLE medico; -- Deleta todos os registros da tabela medico, resetando os autoincrementos
+
+-- Paciente => codigo, nome, email
+--> pk = codigo
+--> codigo = identificador unico interno, autoincremento +1
+----> int, não pode ser nulo, autoincremento, chave primaria
+--> nome = nome do paciente
+----> varchar(200), não pode ser nulo
+--> email = email do paciente
+----> varchar(100), pode ser nulo
+
+CREATE TABLE paciente (
+    codigo INT NOT NULL IDENTITY (1,1),
+    nome VARCHAR(200) NOT NULL,
+    email VARCHAR(100),
+    CONSTRAINT pk_paciente PRIMARY KEY (codigo)
+);
+
+DROP TABLE paciente; -- Deleta a tabela paciente
+
+-- Consulta => codigo medico, codigo paciente, data
+--> fk = codigo medico, codigo paciente
+--> codigo medico = codigo do medico
+----> int, não pode ser nulo, chave estrangeira
+--> codigo paciente = codigo do paciente
+----> int, não pode ser nulo, chave estrangeira
+--> data = data da consulta
+----> date, não pode ser nulo
+--> pk composta = codigo medico, codigo paciente, data
+
+CREATE TABLE consulta (
+    codigo_medicos INT NOT NULL,
+    codigo_pacientes INT NOT NULL,
+    [data] DATE NOT NULL,
+    CONSTRAINT fk_consulta_medico FOREIGN KEY (codigo_medicos) REFERENCES medico (codigo), 
+    -- CONSTRAINT => restrição, 
+    -- FOREIGN KEY => chave estrangeira, 
+    -- (codigo_medicos) => campo que será a chave estrangeira, 
+    -- REFERENCES => referencia a tabela, 
+    -- (medico) => tabela que será referenciada, 
+    -- (codigo) => campo que será referenciado
+    CONSTRAINT fk_consulta_paciente FOREIGN KEY (codigo_pacientes) REFERENCES paciente (codigo),
+    CONSTRAINT pk_consulta PRIMARY KEY (codigo_medicos, codigo_pacientes, [data])
+);
